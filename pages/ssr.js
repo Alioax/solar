49ee0b7233dff6a20fbc69ddbfa159ae5ef4b637
Ali Haghighi <alihghila@gmail.com>
2022-08-17 17:48:55 +0430

    index page updated

--- pages/ssr.js
+++ pages/ssr.js
@@ -1,21 +1,12 @@
-import { useEffect } from "react";
-
 function Dynamic({ data }) {
-    useEffect(() => {
-        console.log('I ran')
-    }, []);
     return (
-        <p>{data.fact}</p>
+        <p className=" text-purple-600">{data.fact}</p>
     )
 }
 
-// This gets called on every request
 export async function getServerSideProps() {
-    // Fetch data from external API
     const res = await fetch(`https://catfact.ninja/fact`)
     const data = await res.json()
-
-    // Pass data to the Dynamic via props
     return { props: { data } }
 }
 
--- pages/static.js
+++ pages/static.js
@@ -1,22 +1,15 @@
-import { useEffect } from "react"
-
 function Static({ data }) {
-  useEffect(() => {
-    console.log('I ran')
-  }, []);
   return (
-    <p>{data.fact}</p>
+    <h1 className="text-3xl font-bold underline">
+      {data.fact}
+    </h1>
   )
 }
 
 export async function getStaticProps() {
   const res = await fetch('https://catfact.ninja/fact')
   const data = await res.json()
-  return {
-    props: {
-      data,
-    },
-  }
+  return { props: { data } }
 }
 
 export default Static
