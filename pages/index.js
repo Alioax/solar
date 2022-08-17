c921c283e08ba74f78806422aa2e4813b83a2611
Ali Haghighi <alihghila@gmail.com>
2022-08-17 18:15:26 +0430

    fact is bold

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ export default function Home({ data }) {
       </Head>
       <main className='container mx-auto px-10 lg:px-96'>
         <div className='flex flex-col justify-center items-center min-h-[90vh]'>
-          <h1 className='text-2xl mr-auto'>{data.fact}</h1>
+          <h1 className='text-2xl mr-auto font-extrabold'>{data.fact}</h1>
           <div className='mr-auto mt-10'>
-            <span className=' text-gray-500'>the complex design was carefully created by the great </span>
-            <span className=' text-rose-800 font-bold'>Moses</span>
+            <span className='text-gray-600'>the complex design was carefully created by the great </span>
+            <span className=' text-cyan-500 font-bold'>Moses</span>
           </div>
         </div>
       </main>
