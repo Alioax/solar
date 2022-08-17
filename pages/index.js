9454c46709a73ce1f5eab41981bbc28ed140b386
Ali Haghighi <alihghila@gmail.com>
2022-08-17 18:05:41 +0430

    moosa changed to moses

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,15 @@ export default function Home({ data }) {
       <Head>
         <title>Cat Facts</title>
         <meta name="description" content="We tell facts about cats." />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/logo.png" />
       </Head>
       <main className='container mx-auto px-10 lg:px-96'>
         <div className='flex flex-col justify-center items-center min-h-[90vh]'>
-          <h1 className='text-2xl'>{data.fact}</h1>
+          <h1 className='text-2xl mr-auto'>{data.fact}</h1>
           <div className='mr-auto mt-10'>
             <span className=' text-gray-500'>the complex design was carefully created by the great </span>
-            <span className=' text-rose-800 font-bold'>Moosa</span>
+            <span className=' text-rose-800 font-bold'>Moses</span>
           </div>
         </div>
       </main>
